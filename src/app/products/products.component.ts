8976cabf2a7ec3dbadf7f8f9e4ae3f7ddfff8c1d
Cris <cris@pop-os.localdomain>
2021-06-13 00:07:17 -0300

    Se agrega la seccion de productos y el servicio correspondiente para consumir el backend. Hay un bug raro que aveces se superpone el paginado con los productos

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
+  products: Product[] = [];
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts(): void {
+    this.productService
+      .getProducts()
+      .subscribe((products) => (this.products = products));
+  }
 }
